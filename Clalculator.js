00396921b0ecaa5f8968916ef4a91d92a10c0bf0
Pradeep <pradeepngowda2003@gmail.com>
2025-10-19 09:33:26 +0530

    Add files via upload

---
+++ Clalculator.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function button(input)
+{
+    display.value += input;
+}
+
+function clearD()
+{
+    display.value = "";
+}
+
+function calc()
+{
+    if()
+    display.value = eval(display.value);
+}
